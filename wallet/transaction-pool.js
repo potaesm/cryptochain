0978a1ecae367d655acc5f0411d56d9127642a9b
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-12-15 00:13:46 +0530

    Add transaction miner

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,6 +1,21 @@
+const Transaction = require("./transaction");
+
 class TransactionPool {
     constructor() {
         this.transactionMap = {};
+    }
+    clear() {
+        this.transactionMap = {};
+    }
+    clearBlockchainTransactions({ chain }) {
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+            for (const transaction of block.data) {
+                if (this.transactionMap[transaction.id]) {
+                    delete this.transactionMap[transaction.id];
+                }
+            }
+        }
     }
     setTransaction(transaction) {
         this.transactionMap[transaction.id] = transaction;
@@ -10,6 +25,9 @@ class TransactionPool {
     }
     setMap(transactionMap) {
         this.transactionMap = transactionMap;
+    }
+    validTransactions() {
+        return Object.values(this.transactionMap).filter(transaction => Transaction.isValidTransaction(transaction));
     }
 }
 
--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,11 +1,12 @@
 const { v1: uuidv1 } = require('uuid');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 const { verifySignature } = require('../util');
 
 class Transaction {
-    constructor({ senderWallet, recipient, amount }) {
+    constructor({ senderWallet, recipient, amount, outputMap, input }) {
         this.id = uuidv1();
-        this.outputMap = this.createOutputMap({ senderWallet, recipient, amount });
-        this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
+        this.outputMap = outputMap || this.createOutputMap({ senderWallet, recipient, amount });
+        this.input = input || this.createInput({ senderWallet, outputMap: this.outputMap });
     }
     static isValidTransaction(transaction) {
         const { input: { address, amount, signature }, outputMap } = transaction;
@@ -19,6 +20,12 @@ class Transaction {
             return false;
         }
         return true;
+    }
+    static rewardTransaction({ minerWallet }) {
+        return new this({
+            input: REWARD_INPUT,
+            outputMap: { [minerWallet.publicKey]: MINING_REWARD }
+        });
     }
     createOutputMap({ senderWallet, recipient, amount }) {
         const outputMap = {};
